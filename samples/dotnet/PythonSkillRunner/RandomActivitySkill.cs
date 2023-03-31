4592f7fa02d4b53456dc5e11acc3f2e150aa4879
Aarón Ch. Sánchez <aarn_100@hotmail.com>
2023-03-31 11:14:21 -0700

    unit tests

--- samples/dotnet/PythonSkillRunner/RandomActivitySkill.cs
+++ samples/dotnet/PythonSkillRunner/RandomActivitySkill.cs
@@ -16,12 +16,13 @@ internal class PythonRandomActivitySkill
 
         [SKFunction("Gets a random activity from an API")]
         [SKFunctionName("GetRandomActivity")]
-        public string GetRandomActivityAsync(string input, SKContext context)
+        public string GetRandomActivityAsync()
         {
             PythonEngine.Initialize();
             using (Py.GIL())
             {
                 dynamic sys = Py.Import("sys");
+                // Env variable PYTHONPATH contains the directory for the pythonskills module
                 var pythonpath = Environment.GetEnvironmentVariable("PYTHONPATH");
                 sys.path.insert(0, pythonpath);
                 dynamic skill = Py.Import("pythonskills.randomActivitySkill");
